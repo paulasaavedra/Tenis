00bf8365c08dd55b20947444fed57dff0e763843
Paula Saavedra <50970099+paulasaavedra@users.noreply.github.com>
2022-11-20 13:50:26 -0300

    Update

--- some_codes/SimpleData.py
+++ some_codes/SimpleData.py
@@ -13,7 +13,7 @@ Sackmann (https://github.com/JeffSackmann).
 
 import pandas as pd
 
-path = 'C:/Users/paula/documents/GitHub/Tenis/some_codes/'
+path = 'C:/Users/paula/Documents/GitHub/Tenis/some_codes/'
 df = pd.read_csv(path + 'atp_matches_2019.csv') 
 
 
@@ -24,3 +24,17 @@ df['age diff'] = abs(df['winner_age'] - df['loser_age'])
 # Winners from a country in particular
 country = 'ARG'
 df_country = df.loc[ df['winner_ioc'] == country]
+
+# Winners from a country and age < 25
+country = 'ARG'
+df_country_age = df.query('winner_ioc == "ARG" & winner_age < 25')
+
+# Total minutes of matches won by ARG players
+df[df['winner_ioc'] == 'ARG']['minutes'].sum()
+
+# Describe
+df[df['winner_name']=='Diego Schwartzman']['minutes'].describe()
+
+#
+df_ages_by_nation = df.groupby('winner_ioc')['winner_age'].mean()
+

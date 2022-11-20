d3e0343074235082b57da2a0401e918557b174c8
Paula Saavedra <50970099+paulasaavedra@users.noreply.github.com>
2022-11-20 15:41:32 -0300

    Update SimpleData.py

--- some_codes/SimpleData.py
+++ some_codes/SimpleData.py
@@ -12,6 +12,7 @@ Sackmann (https://github.com/JeffSackmann).
 
 
 import pandas as pd
+import matplotlib.pyplot as plt
 
 path = 'C:/Users/paula/Documents/GitHub/Tenis/some_codes/'
 df = pd.read_csv(path + 'atp_matches_2019.csv') 
@@ -35,6 +36,50 @@ df[df['winner_ioc'] == 'ARG']['minutes'].sum()
 # Describe
 df[df['winner_name']=='Diego Schwartzman']['minutes'].describe()
 
-#
+# group by
 df_ages_by_nation = df.groupby('winner_ioc')['winner_age'].mean()
 
+# Winners who made the most aces in a match
+aces_max = df.sort_values('w_ace',ascending=False).head(3)
+
+# ARG number of won matches
+arg_won_matches = df[df.winner_ioc == "ARG"].pivot_table(
+    index=["winner_name"],
+    values=["match_num"],
+    aggfunc="count"
+)
+arg_won_matches.sort_values("match_num", ascending=False)
+
+# pivot tables - option 1
+arg_won_levels = df[df.winner_ioc == "ARG"].pivot_table(
+    index=[
+        "winner_name",
+        "loser_name",
+        "tourney_level",
+        "minutes"
+    ],
+    values=["match_num"],
+    aggfunc="count"
+).reset_index()
+
+# pivot tables - option 2
+arg_won_time = df[df.winner_ioc == "ARG"].pivot_table(
+    columns="tourney_level",
+    index="winner_name",
+    values="w_ace",
+    aggfunc="sum"
+)
+
+plt.xticks(rotation=90 )
+plt.plot(arg_won_time)
+plt.show()
+arg_won_time.plot()
+
+
+arg_loser_name = df[df.winner_ioc == "ARG"].pivot_table(
+    columns="round",
+    index="winner_name",
+    values="w_ace",
+    aggfunc="sum"    
+)
+arg_loser_name.plot(figsize=(15,10))
